8691fb2749dce3cdcb1c900633d5bae61c9fcf4e
Ciolpan Sergiu <ciolpan.sergiu64@gmail.com>
2023-09-12 21:57:38 +0300

    add more features and apply a better overall styling

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ const captureBtn = document.getElementById("screenshot-btn");
 const nameField = document.getElementById("name-field");
 const functionField = document.getElementById("function-field");
 const profilePic = document.getElementById("profile-pic-field");
+const newcomeTagField = document.getElementById("newcomer-tag");
+// this is for manual color picking variant
+// const textColorField = document.getElementById("text-color-field");
+const colorPicker = document.getElementById("color-picker");
 
 const card = document.getElementsByClassName("card-container")[0];
 
 const reader = new FileReader();
 
-const employeeName = document.getElementsByClassName("name")[0];
+const employeeName = document.getElementsByClassName("employee-name")[0];
 const employeeFunction = document.getElementsByClassName("function")[0];
 const employeePicture = document.getElementsByClassName("profile-img")[0];
+const newcomerText = document.getElementsByClassName("newcomer-label")[0];
+
+const colorChangingText = document.getElementsByClassName("colored-text");
 
 const fillCard = () => {
   const nameValue = nameField.value;
@@ -18,6 +25,23 @@ const fillCard = () => {
 
   employeeName.innerText = `- ${nameValue} -`;
   employeeFunction.innerText = `- ${functionValue} -`;
+
+  // this is for manual color picking variant
+
+  // for (let i = 0; i < colorChangingText.length; i++) {
+  //   CSS.supports("color", textColorField.value)
+  //     ? (colorChangingText[i].style.color = textColorField.value)
+  //     : (colorChangingText[i].style.color = "black");
+  // }
+
+  for (let i = 0; i < colorChangingText.length; i++) {
+    colorChangingText[i].style.color = colorPicker.value;
+  }
+
+  newcomeTagField.checked
+    ? newcomerText.classList.add("visible-text")
+    : newcomerText.classList.remove("visible-text");
+
   reader.onload = function (e) {
     employeePicture.src = e.target.result;
   };
@@ -32,12 +56,15 @@ const screenshotCard = () => {
 
 fillBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  window.scrollTo(0, 0);
   fillCard();
 });
 
 captureBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  window.scrollTo(0, 55);
+  // this require many trials and errors because
+  // html2canvas take screenshots that are different depending on the window scale
+  // everytime a space is created between an element, the second parameter need some tweeks
+  // in order for screenshot to capture the whole element, without white space;
+  window.scrollTo(0, 30);
   screenshotCard();
 });
